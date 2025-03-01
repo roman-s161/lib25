bc3c6a2c5b0903d7c903b16082b155ce5851effd
roman161 <skokov161@gmail.com>
2025-03-02 00:53:24 +0300

    сделал новые урлы, теперь нужно переделать вьюхи и шаблоны

--- library/urls.py
+++ library/urls.py
@@ -1,28 +1,31 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'library'
 
+book_patterns = [
+    path('book_list/', views.book_list, name='book_list'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('book_detail/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('edit_book/<int:book_id>/edit/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('confirm_delete_book/<int:book_id>/', views.confirm_delete_book, name='confirm_delete_book'),
+    path('edit_book_form/', views.edit_book_form, name='edit_book_form'),
+]
+
+reader_patterns = [
+    path('reader_list/', views.reader_list, name='reader_list'),
+    path('add_reader/', views.add_reader, name='add_reader'),
+    path('reader_detail/<int:reader_id>/', views.reader_detail, name='reader_detail'),
+    path('edit_reader/<int:reader_id>/edit/', views.edit_reader, name='edit_reader'),
+    path('delete_reader/<int:reader_id>/', views.delete_reader, name='delete_reader'),
+    path('confirm_delete_reader/<int:reader_id>/', views.confirm_delete_reader, name='confirm_delete_reader'),
+    path('edit_reader_form/', views.edit_reader_form, name='edit_reader_form'),
+]
 
 urlpatterns = [
-    path('book_list', views.book_list, name='book_list'),
-    path('add_book/', views.add_book, name='add_book'),
-    path('<int:book_id>/', views.book_detail, name='book_detail'),
-    path('<int:book_id>/edit/', views.edit_book, name='edit_book'),
-    path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
-    path('confirm_delete/<int:book_id>/', views.confirm_delete_book, name='confirm_delete_book'),
-
-
-    path('reader_list', views.reader_list, name='reader_list'),
-    path('add_reader/', views.add_reader, name='add_reader'),
-    path('<int:reader_id>/', views.reader_detail, name='reader_detail'),
-    path('<int:reader_id>/edit/', views.edit_reader, name='edit_reader'),
-    path('delete/<int:reader_id>/', views.delete_reader, name='delete_reader'),
-    path('confirm_delete/<int:reader_id>/', views.confirm_delete_reader, name='confirm_delete_reader'),
-
-
+    path('book/', include((book_patterns, 'book'))),
+    path('reader/', include((reader_patterns, 'reader'))),
     path('select_book_reader/', views.select_book_reader, name='select_book_reader'),
-    path('edit_book/', views.edit_book_form, name='edit_book_form'),
-    path('edit_reader/', views.edit_reader_form, name='edit_reader_form'),
     path('', views.index, name='index'),
 ]
--- library/views.py
+++ library/views.py
@@ -49,6 +49,57 @@ def add_reader(request):
         form = ReaderForm()
     return render(request, 'library/add_reader.html', {'form': form})
 
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='Администраторы').exists())
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('library:book_detail', book_id=book_id)
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'library/edit_book.html', {'form': form})
+
+def edit_book_form(request):
+    if request.method == 'POST':
+        form = BookIdForm(request.POST)
+        if form.is_valid():
+            book_id = form.cleaned_data['book_id']
+            if not Book.objects.filter(id=book_id).exists():
+                return render(request, 'library/object_id_not_found.html')
+            return redirect('library:edit_book', book_id=book_id)
+    else:
+        form = BookIdForm()
+    return render(request, 'library/edit_book_form.html', {'form': form})
+
+@login_required
+@user_passes_test(lambda u: u.groups.filter(name='Библиотекари').exists())
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    author = book.author
+    publication_date = book.publication_date
+    return render(request, 'library/book_detail.html', {'book': book, 'author': author, 'publication_date': publication_date})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def select_book_reader(request):
     if request.method == 'POST':
@@ -59,30 +110,20 @@ def select_book_reader(request):
             if selection == 'book':
                 try:
                     book = Book.objects.get(id=book_id)
-                    return redirect('book_detail', book_id=book_id)
+                    return redirect('library:book_detail', book_id=book_id)
                 except Book.DoesNotExist:
-                    return render(request, 'object_id_not_found.html' )
+                    return render(request, 'library/object_id_not_found.html' )
             elif selection == 'reader':
                 try:
                     reader = Reader.objects.get(id=book_id)
-                    return redirect('reader_detail', reader_id=book_id)
+                    return redirect('library:reader_detail', reader_id=book_id)
                 except Reader.DoesNotExist:
-                    return render(request, 'object_id_not_found.html' )
+                    return render(request, 'library/object_id_not_found.html' )
     else:
         form = SelectBookReaderForm()
-    return render(request, 'select_book_reader.html', {'form': form})
+    return render(request, 'library/select_book_reader.html', {'form': form})
 
-def edit_book_form(request):
-    if request.method == 'POST':
-        form = BookIdForm(request.POST)
-        if form.is_valid():
-            book_id = form.cleaned_data['book_id']
-            if not Book.objects.filter(id=book_id).exists():
-                return render(request, 'object_id_not_found.html')
-            return redirect('edit_book', book_id=book_id)
-    else:
-        form = BookIdForm()
-    return render(request, 'edit_book_form.html', {'form': form})
+
 
 def edit_reader_form(request):
     if request.method == 'POST':
@@ -90,11 +131,11 @@ def edit_reader_form(request):
         if form.is_valid():
             reader_id = form.cleaned_data['reader_id']
             if not Reader.objects.filter(id=reader_id).exists():
-                return render(request, 'object_id_not_found.html')
-            return redirect('edit_reader', reader_id=reader_id)
+                return render(request, 'library/object_id_not_found.html')
+            return redirect('library:edit_reader', reader_id=reader_id)
     else:
         form = ReaderIdForm()
-    return render(request, 'edit_reader_form.html', {'form': form})
+    return render(request, 'library/edit_reader_form.html', {'form': form})
 
 
 
@@ -104,16 +145,16 @@ def delete_book(request):
     if request.method == 'POST':
         book_id = request.POST['book_id']
         if not Book.objects.filter(id=book_id).exists():
-            return render(request, 'object_id_not_found.html')
-        return redirect('confirm_delete_book', book_id=book_id)
-    return render(request, 'delete_book.html')
+            return render(request, 'library/object_id_not_found.html')
+        return redirect('library:confirm_delete_book', book_id=book_id)
+    return render(request, 'library/delete_book.html')
 
 def confirm_delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
         book.delete()
-        return redirect('book_list')
-    return render(request, 'confirm_delete_book.html', {'book': book, 'book_id': book_id})
+        return redirect('library:book_list')
+    return render(request, 'library/confirm_delete_book.html', {'book': book, 'book_id': book_id})
 
 @login_required
 @user_passes_test(lambda u: u.groups.filter(name='Администраторы').exists())
@@ -121,16 +162,16 @@ def delete_reader(request):
     if request.method == 'POST':
         reader_id = request.POST['reader_id']
         if not Reader.objects.filter(id=reader_id).exists():
-            return render(request, 'object_id_not_found.html')
-        return redirect('confirm_delete_reader', reader_id=reader_id)
-    return render(request, 'delete_reader.html')
+            return render(request, 'library/object_id_not_found.html')
+        return redirect('library:confirm_delete_reader', reader_id=reader_id)
+    return render(request, 'library/delete_reader.html')
 
 def confirm_delete_reader(request, reader_id):
     reader = get_object_or_404(Reader, id=reader_id)
     if request.method == 'POST':
         reader.delete()
-        return redirect('reader_list')  # Перенаправление на страницу со списком читателей
-    return render(request, 'confirm_delete_reader.html', {'reader': reader, 'reader_id': reader_id})
+        return redirect('library:reader_list')  # Перенаправление на страницу со списком читателей
+    return render(request, 'library/confirm_delete_reader.html', {'reader': reader, 'reader_id': reader_id})
 
 
 
@@ -143,18 +184,6 @@ def confirm_delete_reader(request, reader_id):
 
 
 
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name='Администраторы').exists())
-def edit_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            return redirect('book_detail', book_id=book_id)
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'edit_book.html', {'form': form})
 
 @login_required
 @user_passes_test(lambda u: u.groups.filter(name='Администраторы').exists())
@@ -164,28 +193,21 @@ def edit_reader(request, reader_id):
         form = ReaderForm(request.POST, instance=reader)
         if form.is_valid():
             form.save()
-            return redirect('reader_detail', reader_id=reader_id)
+            return redirect('library:reader_detail', reader_id=reader_id)
     else:
         form = ReaderForm(instance=reader)
-    return render(request, 'edit_reader.html', {'form': form})
+    return render(request, 'library/edit_reader.html', {'form': form})
 
 
 
 
-@login_required
-@user_passes_test(lambda u: u.groups.filter(name='Библиотекари').exists())
-def book_detail(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    author = book.author
-    publication_date = book.publication_date
-    return render(request, 'book_detail.html', {'book': book, 'author': author, 'publication_date': publication_date})
 
 @login_required
 @user_passes_test(lambda u: u.groups.filter(name='Библиотекари').exists())
 def reader_detail(request, reader_id):
     reader = get_object_or_404(Reader, id=reader_id)
     books = reader.books_read.all()
-    return render(request, 'reader_detail.html', {'reader': reader, 'books': books})
+    return render(request, 'library/reader_detail.html', {'reader': reader, 'books': books})
 
 
 
